8bbd7399cebd8ddaa7fc0117d6bf9353fb1f12e8
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-03 17:10:19 +0700

    Edit Chapper 3

--- firstweek/Chappter3PythonOperator.py
+++ firstweek/Chappter3PythonOperator.py
@@ -1,21 +1,21 @@
-# print(5+6)
-# print(5-6)
-# print(13/5)
-# print(13//5)
-# print(13%5)
-# print(3**4) #3*3*3*3
+print(5+6)
+print(5-6)
+print(13/5)
+print(13//5)
+print(13%5)
+print(3**4) #3*3*3*3
 
-# x = 5
-# x+=5
-# print(x)
-# x-=5
-# print(x)
-# x*=5
-# print(x)
-# x/=5
-# print(x)
-# x**=3
-# print(x)
+x = 5
+x+=5
+print(x)
+x-=5
+print(x)
+x*=5
+print(x)
+x/=5
+print(x)
+x**=3
+print(x)
 
 #is, is not, and, or, not
 a=3
