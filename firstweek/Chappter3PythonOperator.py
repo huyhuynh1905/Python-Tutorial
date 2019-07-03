1491f9e97455d07159e1e9e5f0579743a9b45927
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-03 16:56:18 +0700

    Chapper 3 Python Operator

---
+++ firstweek/Chappter3PythonOperator.py
@@ -0,0 +1,30 @@
+# print(5+6)
+# print(5-6)
+# print(13/5)
+# print(13//5)
+# print(13%5)
+# print(3**4) #3*3*3*3
+
+# x = 5
+# x+=5
+# print(x)
+# x-=5
+# print(x)
+# x*=5
+# print(x)
+# x/=5
+# print(x)
+# x**=3
+# print(x)
+
+#is, is not, and, or, not
+a=3
+b=4
+print(a==3 and b==3)
+print(a==3 or b==3)
+print(a is b)
+print(a is not b)
+if(not b==3):
+    print("True")
+else:
+    print("False")
