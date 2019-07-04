83460d8ea5cf21271a775bc29b6bee9168499cb0
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-04 12:53:26 +0700

    Chappter 4 input in python

---
+++ firstweek/Chappter4InputInPython.py
@@ -0,0 +1,12 @@
+# print("Scan x: ")
+# x = input()
+# print("X:",x)
+# print("type x:",type(x))
+#
+# print("Scan y: ")
+# y = int(input())
+# print("y:",y)
+# print("type y:",type(y))
+
+x = input("Scan x:")
+print("X:",x)
