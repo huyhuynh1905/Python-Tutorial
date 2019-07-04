e47aa46cc30e324d374a411c01fe2c1ded4dd9af
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-04 13:46:43 +0700

    Exercise 1 2 3 commit

---
+++ firstweek/Exercise1Circle.py
@@ -0,0 +1,7 @@
+import math
+
+r = float(input("Enter radius: "))
+circumference = 2*r*math.pi
+print("Circumference = ",circumference)
+area = r**2*math.pi
+print("area = ",area)
---
+++ firstweek/Exercise3PointAverage.py
@@ -0,0 +1,8 @@
+m = float(input("Enter your math: "))
+p = float(input("Enter your phys: "))
+c = float(input("Enter your chemi: "))
+
+ave = (m+p+c)/3
+
+print("Average: ",ave)
+print("Round Average: ",round(ave,2))
