a9d11b0e03cd03a4683c91eaff38ff5761869522
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-04 13:23:46 +0700

    Chappter 6 Exception

---
+++ firstweek/Chappter6Exception.py
@@ -0,0 +1,18 @@
+#Syntax Errors
+
+x = 5
+y = 6
+#y + 6 = z
+
+#Runtime Errors
+m = int(input("Input m: "))
+n = int(input("Input n: "))
+print(m,"/",n,"=",m/n)
+
+#try catch
+try:
+    s = 5
+    l = 3
+    k = s/l
+except:
+    print("Error!")
