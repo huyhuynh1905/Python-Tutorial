63b1a9f7cbfce804048fc4e526f175f9f8565478
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-04 13:05:49 +0700

    Chappter 5 Output in Python

---
+++ firstweek/Chappter4OutputInPython.py
@@ -0,0 +1,17 @@
+print('*'*10)
+
+print('{0}-{1}'.format(12,35))
+print('a{0}b{1}c{0}'.format('X','Y'))
+
+print('{0:>4}{1:>15}'.format('index','value'))
+print('{0:>4}{1:>15}'.format(1,10**10))
+print('{0:>4}{1:>15}'.format(2,10**9))
+print('{0:>4}{1:>15}'.format(3,10**8))
+print('{0:>4}{1:>15}'.format(4,10**7))
+print('{0:>4}{1:>15}'.format(5,10**6))
+print('{0:>4}{1:>15}'.format(6,10**5))
+print('{0:>4}{1:>15}'.format(7,10**4))
+print('{0:>4}{1:>15}'.format(8,10**3))
+print('{0:>4}{1:>15}'.format(9,10**2))
+print('{0:>4}{1:>15}'.format(10,10**1))
+
