e64504375c5a15d941111d61788e8ca65e8f764f
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-06 22:54:14 +0700

    Chapter 7 If Else and Pass in Python

--- firstweek/IfElseInPython.py
+++ firstweek/IfElseInPython.py
@@ -3,7 +3,7 @@ if dtb<4:
     print("Bạn rớt môn!")
 print("If...Elif...Else:")
 if dtb>=4 and dtb<=6.5:
-    print("Điểm trung bình")
+    pass #dùng pass để tránh lỗi và ghi nhớ bổ sung code sau
 elif dtb>6.5 and dtb<8.5:
     print("Điểm khá")
 elif dtb>=9.5:
