2651068509cb35ce13e34f06aa4200c746a7df5d
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-08 14:32:06 +0700

    Chapter 10 for loop in python

---
+++ secondweek/ForLoop.py
@@ -0,0 +1,12 @@
+
+for n in range(5):
+    print(n," ")
+print("----------")
+for n in range(1,10):
+    print(n," ")
+print("----------")
+for n in range(1,10,2):
+    print(n," ")
+print("----------")
+for n in range(10,1,-2):
+    print(n," ")
