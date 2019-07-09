5651a6aff3a4bd2f50264f7b1daf31a269e8a5ec
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-09 11:38:23 +0700

    Chapter 14: MathLibra

---
+++ secondweek/MathLibra.py
@@ -0,0 +1,14 @@
+from math import *
+
+print("Abs(): ",abs(-5))
+print("pow(a,b): ",pow(5,3))
+print("sqrt(): ",sqrt(25))
+print("log(): ",log2(2))
+print("log10(): ",log10(100))
+print(degrees(0.1234568))
+print(radians(30))
+
+print("sin(30): ",sin(radians(30)))
+print("cos(30): ",cos(radians(30)))
+print("tan(30): ",tan(radians(30)))
+print("cot(30): ",1/tan(radians(30)))
