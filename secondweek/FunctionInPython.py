0aa953a4b5041883e806d92dbb3cf25e58f6a816
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-11 13:37:21 +0700

    Chapter 18: Function in Python

---
+++ secondweek/FunctionInPython.py
@@ -0,0 +1,13 @@
+def printName(name):
+    print("Hello ",name)
+
+printName("Hcode")
+
+def maxXimum(a,b):
+    if a>b:
+        return a
+    elif a==b:
+        return "Same"
+    else:
+        return b
+print("KQ: ",maxXimum(3,1))
