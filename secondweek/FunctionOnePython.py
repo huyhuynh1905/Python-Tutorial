e2222d5167b3e08ceae0b1c063d6a1adc21148f2
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-12 13:47:43 +0700

    Chapter 22: String function python example 1

---
+++ secondweek/FunctionOnePython.py
@@ -0,0 +1,12 @@
+s = '   Hello, welcome to my tutorial. Hello World!  '
+
+s2 = s.strip()
+print(s.__len__())
+print(s2)
+print(s2.__len__())
+
+print(s.find("e"))
+print(s.rfind("e"))
+
+print(s.count("e",0,20))
+
