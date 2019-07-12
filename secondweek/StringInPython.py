f6454ea750924532e06bc4ac20c4cb6ef474b6ce
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-12 13:31:27 +0700

    Chapter 21: String in python

---
+++ secondweek/StringInPython.py
@@ -0,0 +1,17 @@
+s1 = "Hello World"
+s2 = 'Hello Hcode'
+s3 = '''This Python Tutorial'''
+s4 = """Welcome to my channel"""
+
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+
+print("---------------------------")
+print(s1.upper())
+print(s2.lower())
+print("----------------------------")
+print(s1.rjust(20))
+print(s1.ljust(20,"+"))
+print(s1.center(20,"*"))
