97ec90dcd5d9ad6b8d7d151be0ae54d6fa224f59
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-15 19:34:07 +0700

    Chapter 26: List Function 1

---
+++ thirdweek/FunctionListPython1.py
@@ -0,0 +1,18 @@
+#insert
+list1 = [0,5,6,9]
+print(list1)
+list1.insert(2,10)
+print(list1)
+
+#append
+list2 = [1,3,5,7,9]
+print(list2)
+list2.append(113)
+print(list2)
+
+#remove
+list2.remove(3)
+print(list2)
+
+del list2[1:4]
+print(list2)
