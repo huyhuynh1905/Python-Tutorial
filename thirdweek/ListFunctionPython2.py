fae9e5dac3f7419bdd426c22d8a1c8e4656f670f
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-15 19:43:40 +0700

    Chapter 27: List Function 2

---
+++ thirdweek/ListFunctionPython2.py
@@ -0,0 +1,14 @@
+#Reverse
+list1 = [0,1,3,5,7,9]
+list2 = reversed(list1)
+print(list1)
+for i in list2:
+    print(i,end="\t")
+
+#sort
+print()
+print("*"*20)
+list3 = [5,1,6,4,2,8,9]
+print(list3)
+list3.sort()
+print(list3)
